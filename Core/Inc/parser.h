2e198db23fa41981494eba85b8ba585df83eb8c6
Zaytsev Artem <61655092+nentu@users.noreply.github.com>
2025-10-06 14:40:11 +0300

    Add parser interface

---
+++ Core/Inc/parser.h
@@ -0,0 +1,32 @@
+#ifndef __Parser_H
+#define __Parser_H
+
+// State list
+
+#define CHNG_E 0
+#define CHNG_S 1
+#define CHNG_T 2
+#define CHNG__ 3
+#define CRT_DIGIT 4
+#define CRT_E 5
+#define CRT_N 6
+#define CRT_W 7
+#define CRT__ 8
+#define LINE 9
+#define START 10
+
+
+enum ParserRes{
+	PRSR_CREATE_SCHEME,
+	PRSR_CHANGE_SCHEME,
+	PRSR_INTER,
+	PRSR_OK,
+	ERROR,
+};
+
+
+extern char parser_buffer[50];
+
+ParserRes parser_step(char input_symbol);
+
+#endif
