ec2f815221a06094888caccf26731d651689bfcc
belovlaska <forolimpvk@gmail.com>
2025-11-25 16:23:55 +0300

    message added

--- Core/Src/color_controller.c
+++ Core/Src/color_controller.c
@@ -23,17 +23,25 @@ void change_color(char input_symbol) {
     if(cur_state == WORK){
         if ( input_symbol == '0'){
             clear_lights();
+            sprintf(write_buffer, "\nNo lights\n");
+            write_string(write_buffer);
         }
         else if (input_symbol == '\n'){
             cur_state = SETUP;
+            sprintf(write_buffer, "\nSwitch to setup mode\n");
+            write_string(write_buffer);
         }
         else if (input_symbol > '0' && input_symbol <= '9'){
             set_light_power(color_model[(input_symbol - '0')].color, color_model[(input_symbol-'0')].power);
+            sprintf(write_buffer, "\n %d Color %d power\n", color_model[(input_symbol - '0')].color, color_model[(input_symbol-'0')].power);
+            write_string(write_buffer);
         }
     }
     else if (cur_state == SETUP){
         cur_color_state = input_symbol-'0';
         cur_state = COLOR;
+        sprintf(write_buffer, "\n %d state choosed\n", cur_color_state);
+        write_string(write_buffer);
     }
     else if (cur_state == COLOR) {
         if (input_symbol  == 'a'){
@@ -47,6 +55,8 @@ void change_color(char input_symbol) {
         }
         color_model[cur_color_state].color = cur_color;
         cur_state = POWER;
+        sprintf(write_buffer, "\n %d color choosed\n", cur_color);
+        write_string(write_buffer);
     }
     else if (cur_state == POWER) {
         if (input_symbol == '\n'){
@@ -54,9 +64,13 @@ void change_color(char input_symbol) {
         }
         else if( input_symbol == '+'){
             color_model[cur_color_state].power += 10;
+            sprintf(write_buffer, "\n Power + 10\n");
+            write_string(write_buffer);
         }
         else if( input_symbol == '-'){
             color_model[cur_color_state].power -= 10;
+            sprintf(write_buffer, "\n Power - 10\n");
+            write_string(write_buffer);
         }
     }
 
