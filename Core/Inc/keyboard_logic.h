81958b557f67633898472e95c5e6fca523089566
nentu <zajtsev.artjom@samlit.net>
2025-11-28 12:42:22 +0300

    Add interfaces for driver and logic

---
+++ Core/Inc/keyboard_logic.h
@@ -0,0 +1,13 @@
+/*
+ * keyboard_logic.h
+ *
+ *  Created on: Nov 28, 2025
+ *      Author: zam12
+ */
+
+#ifndef INC_KEYBOARD_LOGIC_H_
+#define INC_KEYBOARD_LOGIC_H_
+
+
+
+#endif /* INC_KEYBOARD_LOGIC_H_ */
