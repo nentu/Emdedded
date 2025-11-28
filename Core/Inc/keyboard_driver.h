1201f5de3c955f9686ecd4a83a13438840ce7aea
nentu <zajtsev.artjom@samlit.net>
2025-11-28 14:57:28 +0300

    Smth is working

--- Core/Inc/keyboard_driver.h
+++ Core/Inc/keyboard_driver.h
@@ -12,7 +12,11 @@
 
 #define KB_INPUT_REG 0x0
 #define KB_OUTPUT_REG 0x1
+#define KB_POLARITY_REG 0x2
 #define KB_CONFIG_REG 0x3
+
+
+#include "i2c.h"
 
 /*
 btn_id: 
@@ -20,6 +24,6 @@ btn_id:
 - 0-11 btn id which is clicked
 */
 int read_keyboard();
-int kb_read_row(I2C_HandleTypeDef * i2c, uint8_t row, uint8_t *res);
-
+void kb_read_row(I2C_HandleTypeDef * i2c, uint8_t row, uint8_t *res);
+void kb_init(I2C_HandleTypeDef * i2c);
 #endif /* INC_KEYBOARD_H_ */
--- Core/Src/keyboard_driver.c
+++ Core/Src/keyboard_driver.c
@@ -8,29 +8,35 @@
 
 #include "keyboard_driver.h"
 #include <stdint.h>
-#include "i2c.h"
+//#include "i2c.h"
 
-
-void kb_write_reg(I2C_HandleTypeDef * i2c, uint16_t MemAddress, uint8_t data) {
-    HAL_I2C_Mem_Write(i2c, KEYBOARD_DEVICE, MemAddress, 1, &data, 1, 100);
+HAL_StatusTypeDef kb_write_reg(I2C_HandleTypeDef * i2c, uint16_t MemAddress, uint8_t data) {
+    return HAL_I2C_Mem_Write(i2c, KEYBOARD_DEVICE & 0xfffe, MemAddress, I2C_MEMADD_SIZE_8BIT, &data, 1, 10);
 }
 
-void kb_read_reg(I2C_HandleTypeDef * i2c, uint16_t MemAddress, uint8_t* data) {
-    HAL_I2C_Mem_Read(i2c, KEYBOARD_DEVICE, MemAddress, 1, data, 1, 100);
+void kb_init(I2C_HandleTypeDef * i2c){
+
+	kb_write_reg(i2c, KB_CONFIG_REG, 0b1110000);
+	kb_write_reg(i2c, KB_POLARITY_REG, 0);
+	kb_write_reg(i2c, KB_OUTPUT_REG, 0xf);
 }
 
 
-void kb_write_config(I2C_HandleTypeDef * i2c, uint8_t data) {
-	kb_write_reg(i2c, KB_CONFIG_REG, data);
+
+
+HAL_StatusTypeDef kb_read_reg(I2C_HandleTypeDef * i2c, uint16_t MemAddress, uint8_t* data) {
+    return HAL_I2C_Mem_Read(i2c, KEYBOARD_DEVICE | 1, MemAddress, I2C_MEMADD_SIZE_8BIT, data, 1, 10);
 }
 
 void kb_select_row(I2C_HandleTypeDef * i2c, uint8_t row) {
-	kb_write_config(i2c, ~((uint8_t) (1 << row)));
+	uint8_t bin_mask = (uint8_t) (1 << row);
+	kb_write_reg(i2c, KB_CONFIG_REG, ~bin_mask);
+	kb_write_reg(i2c, KB_OUTPUT_REG, ~bin_mask);
 }
 
 
 void kb_read_row(I2C_HandleTypeDef * i2c, uint8_t row, uint8_t *res){
 	kb_select_row(i2c, row);
-	kb_write_reg(i2c, KB_OUTPUT_REG, (uint8_t) (1 << row));
+	HAL_Delay(1);
 	kb_read_reg(i2c, KB_INPUT_REG, res);
 }
