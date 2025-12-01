7401d3547c9da42456787a9a4f6d690cbe8e538c
nentu <zajtsev.artjom@samlit.net>
2025-12-01 11:40:51 +0300

    Add tick loop

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -132,19 +132,20 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
     
-    //TODO read and parse keyboard input
     GPIO_PinState btn_state = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
-    if ( btn_state == GPIO_PIN_RESET) {
-      if (mode == 1) {
-        mode = 0;
-      }
-      else{
-        mode = 1;
-      }
+
+    if (HAL_GetTick() - tick_count >= 100) {
+        if ( btn_state == GPIO_PIN_RESET) {
+        	mode = 1 - mode;
+        }
+
+    	tick_count = HAL_GetTick();
+    	btn_id = read_keyboard(&hi2c1);
+    	if (btn_id == -1) continue;
+		keyboard(btn_id, mode);
+
+
     }
-    btn_id = read_keyboard();
-    if (btn_id == -1) continue;
-    keyboard(btn_id, mode);
 
       // HAL_StatusTypeDef read_res = read_char(&input_symbol);
       // if (read_res != HAL_OK)
